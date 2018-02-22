2d7401312b052bc6460d4b02f5455b9f6b6b449f
alex <ach1@vt.edu>
2018-02-22 15:40:37 -0500

    major clean-up and re-write of clunky code.

--- eulerThree/eulerThree.py
+++ eulerThree/eulerThree.py
@@ -1,47 +1,26 @@
 #determines if a given number is isPrime
 #a - number to check prime.
-def isPrime(a):
+def gpfFinder(a):
     a = abs(int(a))
+    gpf = 2
 
-    if a < 2:
-        return False
-    if a == 2:
-        return True
-    if a>2 and a%2 == 0:
-        return False
-    for x in range(3, int(a**0.5) + 2, 2):
-        if a % x == 0:
-            return False
-        return True
-
-#returns the factors of a given number.
-#if given number is prime, returns 0
-#b - number to factor.
-def factors(b):
-    listFactors = []
-
-    for i in range(2, int(b**0.5)+1):
-        if b%i == 0:
-            listFactors.append(int(b/i))
-            #listFactors.append(int(i)) #for more context de-comment
-
-    return listFactors
-    
-#main function. calls isPrime on factors list result to filter
-#and max() to determine greatest prime factor from user input.
-def main():
-    number = int(input('Enter number to test: '))
-    listNumber = factors(number)
-
-    if len(listNumber) == 0:
-        print("input is prime. largest prime factor is %d." % number)
+    if a < 2: # 1 and 0 are not prime
+        print("1 and 0 do not have prime factors.")
+        raise SystemExit
+    elif a == 2: # 2 is smallest prime and only even prime
+        return a
     else:
-        for z in range(0, len(listNumber)):
-            if isPrime(listNumber[z]) == False:
-                listNumber.pop([z])
+        while a > gpf:
+            if a%gpf == 0:
+                a = a/gpf
+            else:
+                gpf += 1
+        return gpf
 
-        print("largest prime factor is %d" % int(max(listNumber)))
+#------------------------------------------------------
+def main():
+    usrIn = int(input("enter number: "))
+    print("greatest prime factor of %d is %d" %(usrIn, gpfFinder(usrIn)))
 
-# main execution conditional
 if __name__ == "__main__":
     main()
