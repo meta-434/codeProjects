b2fe117d9ccb11694fa8564511c2cb509c971407
alex <ach1@vt.edu>
2018-02-28 15:50:26 -0500

    add test_eulerFour

---
+++ euler4/test_eulerFour.py
@@ -0,0 +1,11 @@
+import unittest
+import eulerFour
+
+class TestEulerFour(unittest.TestCase):
+
+    def test_MAIN(self):
+        #result = eulerThree.gpfFinder(600851475143)
+        #self.assertEqual(result, 6857)
+
+if __name__ == "__main__":
+    unittest.main()
