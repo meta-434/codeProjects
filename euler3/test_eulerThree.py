895f9f54e1962f2db30e7c71436be1e9ed2df5be
alex <ach1@vt.edu>
2018-03-01 14:42:35 -0500

    completed test_eulerThree.py

--- euler3/test_eulerThree.py
+++ euler3/test_eulerThree.py
@@ -7,5 +7,14 @@ class TestEulerThree(unittest.TestCase):
         result = eulerThree.gpfFinder(600851475143)
         self.assertEqual(result, 6857)
 
+        lowest = eulerThree.gpfFinder(2)
+        self.assertEqual(lowest, 2)
+
+        with self.assertRaises(SystemExit): eulerThree.gpfFinder(0)
+
+        absChecker = eulerThree.gpfFinder(-21)
+        self.assertEqual(absChecker, 7)
+
+
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity = 0, buffer = True)
