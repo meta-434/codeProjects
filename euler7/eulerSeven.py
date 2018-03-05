a7324fad136a1ce77f9cf232ba1c8acf8f7d9249
alex <ach1@vt.edu>
2018-03-05 16:52:12 -0500

    init for #7

---
+++ euler7/eulerSeven.py
@@ -0,0 +1,17 @@
+def primeFinder(endVal):
+
+    counter = 13
+    primeList = [2, 3, 5, 7, 11, 13]
+    length = len(primeList)
+    # WHERE DO I PUT THIS i = -1
+
+    while length < 10001
+        if counter % primeList[-i] == 0:
+
+
+def main():
+
+    print(primeFinder())
+
+if __name__ == "__main__":
+    main()
