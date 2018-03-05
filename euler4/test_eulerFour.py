3670df4014888077aea5d939be335090dd259bd6
alex <ach1@vt.edu>
2018-03-05 12:33:37 -0500

    unoptimized test method for solution method 2 euler project 4.

--- euler4/test_eulerFour.py
+++ euler4/test_eulerFour.py
@@ -3,9 +3,13 @@ import eulerFour
 
 class TestEulerFour(unittest.TestCase):
 
-    def test_MAIN(self):
+    #def test_digits(self):
         #result = eulerThree.gpfFinder(600851475143)
         #self.assertEqual(result, 6857)
-
+    #def test_reverse(self):
+    def test_palindromic(self):
+        given = int(eulerFour.palindromic())
+        reverse = int(eulerFour.reverse(eulerFour.palindromic()))
+        self.assertEqual(given, reverse)
 if __name__ == "__main__":
     unittest.main()
