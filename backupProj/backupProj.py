144fbf44e746ea00b271c5ae12512d87fa110281
alex <ach1@vt.edu>
2018-05-31 13:28:38 -0400

    reworking backup class

--- backupProj/backupProj.py
+++ backupProj/backupProj.py
@@ -12,10 +12,18 @@ build = 'v0.2a1'
 now = datetime.datetime.now()
 
 class payload:
-    def __init__(self, version, source, lastBackup):
-        self.version = version
+    def __init__(self, id, source, time):
         self.source = source
-        self.lastBackup = lastBackup
+        self.id = id
+        self.time = date
+
+    def writeToPrefs(self):
+        self.id = id_generator()
+        self.now = str(now)
+
+        with open('prefs.txt', 'a') as f:
+            f.write('\n' + self.id  + ' | ' + self.source + ' | ' + self.now)
+            f.close()
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
@@ -45,37 +53,13 @@ def init():
     locisSrcRemote = False
     locisTgtRemote = False
 
-#    prefs = input('Is there a prefs.txt file to import for an existing backup? y/N: ')
-#    if prefs == 'y' or prefs == 'Y':
-#        f = open('prefs.txt', 'r')
-#        f1 = f.readlines()
-#        for x in f1:
-#            print(x)
-#
+    #!!!existPrefs = bool(input)
+    payloadPath = input('Enter the full filepath to the source enclosing folder or single file: ')
 
-            payloadPath = input('Enter the full filepath to the source enclosing folder or single file: ')
-            id_gen_hold = id_generator()
-            namer = str(id_gen_hold)
 
-            id_gen_hold = BackupItem(payloadPath, now.isoformat())
 
-                with open('prefs.txt', 'a') as f:
-                    f.write('\n' + namer + ','+ str(id_gen_hold.version) + ',' + id_gen_hold.source + ',' + str(id_gen_hold.isSrcRemote) + ',' + id_gen_hold.target + ',' + str(id_gen_hold.isTgtRemote) + ',' + id_gen_hold.lastBackup)
-                    f.close()
-            elif whereTarget == 'n' or whereTarget == 'N':
-                print('Remote target backup functionality has not yet been implemented... sorry.')
 
-        elif whereSource == 'n' or whereSource == 'N':
 
-            #if locisSrcRemote = True
-                #locSrcPath = input('Enter full SSH address to source enclosing folder: ')
-
-            #locTgtPath
-            print('Remote source backup functionality has not yet been implemented... sorry.')
-
-    else:
-        print('input not recognized...')
-        return -1
 
 
 def payloadLocation():
